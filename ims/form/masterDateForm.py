91cbe500e77189e9d95093bda5ad4640a021dd7a
yo <shuayayjb@gmail.com>
2019-11-12 00:22:27 +0900

    edit masterData template

---
+++ ims/form/masterDateForm.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, validators
+
+class UserForm(FlaskForm):
+    userId = StringField('ユーザID',[validators.Length(min=4, max=25, message=u'4-25文字で入力してください.')])
+    password = PasswordField('パスワード',[validators.Length(min=4, max=25, message=u'4-25文字で入力してください.')])
